d8d4b7e2b3658dbc2332026b9b6015c8587e80dc
Zully Suarez <xzullysuarezx@gmail.com>
2026-05-22 20:50:21 -0500

    config: agregar librerias de vistas

--- fronted/app/build.gradle.kts
+++ fronted/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.gson)
@@ -52,4 +53,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
