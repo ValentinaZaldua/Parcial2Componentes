b18f47448f1c30ea3b968f5a8dc7bf32b518aa44
Zully Suarez <xzullysuarezx@gmail.com>
2026-05-22 21:05:53 -0500

    feat: eliminar libreria

--- fronted/app/src/main/java/com/example/parcial2pc/presentation/ui/theme/Theme.kt
+++ fronted/app/src/main/java/com/example/parcial2pc/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.ud.riddle.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
