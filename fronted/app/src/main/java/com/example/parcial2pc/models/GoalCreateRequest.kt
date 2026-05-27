813da95c91917945571aa848fcab30ab1630c741
CAMILOACGC <camiloandresclavijoguerrero@gmail.com>
2026-05-27 12:07:11 -0500

    Corrección del GoalCreateRequest.kt para imagen y calendario

--- fronted/app/src/main/java/com/example/parcial2pc/models/GoalCreateRequest.kt
+++ fronted/app/src/main/java/com/example/parcial2pc/models/GoalCreateRequest.kt
@@ -1,13 +1,10 @@
 package com.example.parcial2pc.models
 
-import com.google.gson.annotations.SerializedName
-
 data class GoalCreateRequest(
     val name: String,
     val description: String,
     val totalValue: Double,
     val targetDate: String,
     val imageUrl: String? = null,
-    @SerializedName("membersReq")
     val members: List<String>
 )
