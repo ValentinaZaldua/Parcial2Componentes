0444988394955a390dae5648cc056e7fa3606e7f
Valentina Zaldúa <laurazaldua1302@hotmail.com>
2026-05-26 22:35:06 -0500

    Se añadieron glades

--- fronted/app/build.gradle.kts
+++ fronted/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.ud.riddle"
+    namespace = "com.example.parcial2pc"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.ud.riddle"
+        applicationId = "com.example.parcial2pc"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -42,10 +42,13 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.gson)
     implementation(libs.kotlinx.coroutines.android)
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("com.squareup.picasso:picasso:2.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
