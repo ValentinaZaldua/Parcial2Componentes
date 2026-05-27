a9b78ca8d1e5785d1db02435f2356e69a21580ad
CAMILOACGC <camiloandresclavijoguerrero@gmail.com>
2026-05-26 23:27:33 -0500

    Actualización GoalState.kt

--- fronted/app/src/main/java/com/example/parcial2pc/models/states/GoalState.kt
+++ fronted/app/src/main/java/com/example/parcial2pc/models/states/GoalState.kt
@@ -1,10 +1,10 @@
-package com.ud.riddle.models.states
+package com.example.parcial2pc.models.states
 
 // Igual al patrón GameUiState del proyecto original.
 // Representa los posibles estados de la pantalla.
 sealed class GoalState {
     object Idle : GoalState()
     object Loading : GoalState()
-    data class Success(val goals: List<com.ud.riddle.models.Goal>) : GoalState()
+    data class Success(val goals: List<com.example.parcial2pc.models.Goal>) : GoalState()
     data class Error(val message: String) : GoalState()
 }
